d3383fbc4a641f650e3c20e792dabf7899d709b3
AndrewS-GH <66842882+AndrewS-GH@users.noreply.github.com>
2023-12-06 20:39:03 -0500

    Create day06.py

---
+++ day06.py
@@ -0,0 +1,39 @@
+import re
+from functools import reduce
+import operator
+
+get_ints_pattern = r'\b\d+\b'
+
+
+def valid_wait_time(time, distance, wait_time):
+    # wait time = speed
+    time -= wait_time
+    return (wait_time * time) > distance
+
+
+def main():
+    # Part 1
+    with open('input', 'r') as fp:
+        times = [int(v) for v in re.findall(get_ints_pattern, fp.readline())]
+        distances = [int(v) for v in re.findall(get_ints_pattern, fp.readline())]
+
+    wins = []
+    for time, distance in zip(times, distances):
+        total_wins = sum(1 if valid_wait_time(time, distance, i) else 0 for i in range(1, time))
+        wins.append(total_wins)
+
+    result = reduce(operator.mul, wins)
+    print(result)
+
+    # Part 2
+    # sorry about brute forcing day 2
+    # maybe later I will come back and figure out the optimal algorithm (probably not)
+    with open('input', 'r') as fp:
+        time = int("".join(re.findall(get_ints_pattern, fp.readline())))
+        distance = int("".join(re.findall(get_ints_pattern, fp.readline())))
+
+    total_wins = sum(1 if valid_wait_time(time, distance, i) else 0 for i in range(1, time))
+    print(total_wins)
+
+
+main()
