d1584aeb14af0cca1cacfb5804e6842dcbbfb206
AndrewS-GH <66842882+AndrewS-GH@users.noreply.github.com>
2023-12-07 23:44:23 -0500

    Update day06.py

--- day06.py
+++ day06.py
@@ -19,20 +19,19 @@ def main():
 
     wins = []
     for time, distance in zip(times, distances):
-        total_wins = sum(1 if valid_wait_time(time, distance, i) else 0 for i in range(1, time))
+        total_wins = sum(valid_wait_time(time, distance, i) for i in range(1, time))
         wins.append(total_wins)
 
     result = reduce(operator.mul, wins)
     print(result)
 
     # Part 2
-    # sorry about brute forcing part 2
     # maybe later I will come back and figure out the optimal algorithm (probably not)
     with open('input', 'r') as fp:
         time = int("".join(re.findall(get_ints_pattern, fp.readline())))
         distance = int("".join(re.findall(get_ints_pattern, fp.readline())))
 
-    total_wins = sum(1 if valid_wait_time(time, distance, i) else 0 for i in range(1, time))
+    total_wins = sum(valid_wait_time(time, distance, i) for i in range(1, time))
     print(total_wins)
 
 
